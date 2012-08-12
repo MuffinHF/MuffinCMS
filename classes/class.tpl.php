f1aaac99a4b1b0f7c9bfd6513d4777070ef13429
MuffinHF <09wilce@gmail.com>
2012-08-12 20:14:02 +0400

    Initial commit.

---
+++ classes/class.tpl.php
@@ -0,0 +1,19 @@
+<?php
+namespace mTemplate;
+if(!defined('VISIBLE')) { die('You can\'t access this file.'); }
+
+class display
+{
+    public function initiate()
+    {
+        
+    }
+    public function getPage()
+    {
+        global $tpl->page;
+        
+    }
+    
+}
+
+?>
---
+++ global.php
@@ -0,0 +1,13 @@
+<?php
+if(!defined('VISIBLE')) { die('You can\'t access this file.'); }
+
+if(isset($_SERVER['HTTP_CF_CONNECTING_IP'])) { $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP']; }
+
+define('C', 'classes/');
+define('T', 'tpl/');
+
+require_once C . 'class.tpl.php';
+require_once C . 'class.core.php';
+
+$tpl = new mTemplate\display;
+?>
